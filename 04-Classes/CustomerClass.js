deb8f41ae8423909b013dd0de35bbc0d18af709d
VeeraEzhilVendhan <veeraezhilvendhan.v@gmail.com>
2023-04-12 21:32:28 +0530

    Added getters and setters

--- 04-Classes/CustomerClass.js
+++ 04-Classes/CustomerClass.js
@@ -5,9 +5,24 @@ var Customer = /** @class */ (function () {
         this.name = cusname;
         this.id = cusid;
     };
+    Customer.prototype.getName = function () {
+        return this.name;
+    };
+    Customer.prototype.getId = function () {
+        return this.id;
+    };
+    Customer.prototype.setId = function (cusid) {
+        this.id = cusid;
+    };
+    Customer.prototype.setName = function (cusname) {
+        this.name = cusname;
+    };
     return Customer;
 }());
 var customer = new Customer();
 customer.id = 5;
 customer.name = "arun";
+console.log(customer);
+customer.setId(6);
+customer.setName("varun");
 console.log(customer);
--- 04-Classes/CustomerClass.ts
+++ 04-Classes/CustomerClass.ts
@@ -1,14 +1,35 @@
 class Customer{
+
+     //default access modifier is public - public name:string
      name:string;
      id:number;
 
-     Customer(cusname:string, cusid:number){
+    Customer(cusname:string, cusid:number){
         this.name=cusname;
         this.id=cusid;
-     }
+    }
+
+    public getName():string{
+        return this.name;
+    }
+
+    public getId():number{
+        return this.id;
+    }
+
+    public setId(cusid:number):void{
+        this.id=cusid;
+    }
+
+    public setName(cusname:string):void{
+        this.name=cusname;
+    }
 }
 
 let customer=new Customer();
 customer.id=5;
 customer.name="arun";
+console.log(customer);
+customer.setId(6);
+customer.setName("varun");
 console.log(customer);
